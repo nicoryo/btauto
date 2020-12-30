67aa18d4324a7f918dadc178159dbcdd89329cdd
nicoryo <nicoryo.giogio@gmail.com>
2020-12-30 15:27:29 +0900

    1st commit

---
+++ getTicker.py
@@ -0,0 +1,69 @@
+import pybitflyer
+import mysql.connector as mydb
+from datetime import datetime as dt
+from time import sleep
+
+# APIへアクセス
+api = pybitflyer.API(
+  # api_key="UbaTVuzy11W2hsooUS4i77", api_secret="8xOWjqX5jbgW0SRc8D+8h21//hnmDp984oBve1HEjZI="
+  )
+# 取得するデータを選択する
+option = ['timestamp','ltp']
+
+# best_ask：最高買価格
+# best_bid：最低売価格
+# best_ask_size：最高買価格数
+# best_bid_size：最低売価格数
+# ltp：最終取引価格
+# total_ask_depth：買注文総数
+# total_bid_depth：売注文総数
+# volume_by_product: 価格ごとの出来高
+
+
+# 取得間隔(秒)
+interval = 60
+
+# Mysqlへのコネクションの作成
+conn = mydb.connect(
+    host='localhost',
+    port='3306',
+    user='root',
+    password='',
+    database='bitflyer'
+)
+# カーソルを取得する
+cur = conn.cursor()
+
+print('準備完了')
+
+while True:
+  # 取得
+  ticker = api.ticker(product_code="BTC_JPY")
+
+  # 保存用にコンマ区切りにする
+  line = ','.join([str(ticker[op]) for op in option])
+  print(line)
+
+  # レコード追加のSQL文
+  add_bttable =("INSERT INTO 1min_table "
+                "(timestamp, price) "
+                "VALUES (%s, %s)"
+                )
+
+  try:
+    btdate = dt.strptime(ticker['timestamp'], '%Y-%m-%dT%H:%M:%S.%f')
+  except:
+    btdate = dt.strptime(ticker['timestamp'], '%Y-%m-%dT%H:%M:%S')
+ 
+  # パラメータの設定
+  btdata =  (
+      btdate,
+      ticker['ltp']
+    )
+
+  # SQL文の実行
+  cur.execute(add_bttable, btdata)
+  conn.commit()
+
+  # 指定した秒数だけストップ
+  sleep(interval)
---
+++ mysqlpy.py
@@ -0,0 +1,54 @@
+import mysql.connector as mydb
+
+# コネクションの作成
+conn = mydb.connect(
+    host='localhost',
+    port='3306',
+    user='root',
+    password='',
+    database='bitflyer'
+)
+
+# カーソルを取得する
+cur = conn.cursor()
+
+# テーブルを作成する
+cur.execute(
+    """
+    CREATE TABLE IF NOT EXISTS 1min_table(
+        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        timestamp DATETIME,
+        price INT
+        )
+    """
+)
+conn.commit()
+
+cur.execute(
+    """
+    CREATE TABLE IF NOT EXISTS 5min_table(
+        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        timestamp DATETIME,
+        sig VARCHAR(25),
+        start INT,
+        end INT,
+        max INT,
+        min INT
+        )
+    """
+)
+conn.commit()
+
+# 書き込みテスト
+# cur.execute("INSERT INTO 1min_table (timestamp, price) VALUES (20201226, 10200)")
+# conn.commit()
+
+# MySQL出力テスト
+# cur.execute("SELECT * FROM 1min_table;")
+# rows = cur.fetchall()
+# 出力
+# for i in rows:
+#     print(i)
+
+cur.close
+conn.close
